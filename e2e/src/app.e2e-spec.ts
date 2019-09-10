6b87322363e116d0520bb1dba5fdf687c287b9c3
bikrant.kataria <bikrant.kataria@CORP.AMAZONWORKSPACES.COM>
2019-09-10 19:14:45 +1200

    initial version

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('bik-app app is running!');
+    expect(page.getTitleText()).toEqual('bik-app12 app is running!');
   });
 
   afterEach(async () => {
